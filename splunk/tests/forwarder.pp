4742ffde40270acf2b584ed6f97de52374924eb1
Cody Herriges <c.a.herriges@gmail.com>
2011-02-04 14:13:33 +0000

    More testing.

--- splunk/tests/forwarder.pp
+++ splunk/tests/forwarder.pp
@@ -1,25 +1,26 @@
-class { "splunk::users":
+class { 'splunk::users':
   virtual => false
 }
 
-class { "splunk::package":
-  pkg_base => "http://tia.cat.pdx.edu",
-  pkg_file => "splunk-4.1.6-89596-linux-2.6-x86_64.rpm",
+class { 'splunk::package':
+  pkg_base => 'http://tia.cat.pdx.edu',
+  pkg_file => 'splunk-4.1.6-89596-linux-2.6-x86_64.rpm',
   has_repo => false,
 }
 
-class { "splunk::service": }
+class { 'splunk::service': }
 
-class { "splunk": }
-class { "splunk::app": }
-class { "splunk::lwf": }
-class { "splunk::inputs": }
+class { 'splunk': }
+class { 'splunk::app': }
+class { 'splunk::lwf': }
+class { 'splunk::inputs': }
 splunk::inputs::target {
-  "yum":
-    target => "/var/log/yum.log",
+  'yum':
+    target => '/var/log/yum.log',
 }
-class { "splunk::outputs::global": }
-#splunk::outputs::fragment {
-#  "splunk":
-#    port => "9999",
-#}
+class { 'splunk::outputs': }
+class { 'splunk::outputs::global': }
+splunk::outputs::server {
+  'splunk':
+    port => '9999',
+}
