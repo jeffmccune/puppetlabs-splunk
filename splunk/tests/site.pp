8320ea04bf6d13c5afb0978415b361f53e5e99dc
Cody Herriges <c.a.herriges@gmail.com>
2011-02-08 18:02:41 +0100

    Typo

--- splunk/tests/site.pp
+++ splunk/tests/site.pp
@@ -11,16 +11,16 @@ class { 'splunk::app': }
 class { 'splunk::inputs': }
 class { 'splunk::inputs::ssl':
   server_cert     => '$SPLUNK_HOME/etc/auth/server.pem',
-  password        => 'password'
+  password        => 'password',
   root_ca         => '$SPLUNK_HOME/etc/auth/cacert.pem',
-  validate_client => true
+  validate_client => true,
 }
 splunk::inputs::target { 'messages':
-    target => '/var/log/messages',
+  target => '/var/log/messages',
 }
 splunk::inputs::target { 'maillog':
-    target => '/var/log/maillog';
+  target => '/var/log/maillog';
 }
 splunk::inputs::receiver { '9999':
-  ssl      => true,
+  ssl => true,
 }
