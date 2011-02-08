8e80407b001c9127665840de85dfea116e6084ff
Cody Herriges <c.a.herriges@gmail.com>
2011-02-08 20:36:52 +0100

    Licensing, Documentation, Code smell.
      Included the Apache 2.0 License now that I am approaching
      publishability.  Once manifest documented...a bunch more to go.
      Doing code smell as I write documentation to produce a unified
      style.

--- splunk/manifests/app.pp
+++ splunk/manifests/app.pp
@@ -1,18 +1,48 @@
 # Define: splunk::app
 #
-#   Creates the initial app.conf and it's parents directories for the portion of
+#   Creates the initial app.conf and it's parent directories for the portion of
 #   Splunk being managed by Puppet.
 #
-#   Cody Herriges <cody@puppetlabs.com>
-#   2011-1-07
+# License:
+#
+#   Licensed under Apache 2.0.  Full license text can be found in LICENSE which
+#   is shipped as part of this module, in it's root directory.
 #
 # Parameters:
 #
-# Actions:
+#   Default parameters are sane enough that someone will have a fully functional
+#   installation without having to use the parameterized class syntax.
+#
+#   - **enable**
+#       If the app you configured with Puppet is actually enabled in Splunk.
+#       Any config stored in this app with not function unless that app is
+#       enabled.
+#
+#   - **ensure**
+#       This simply controls the existance of the app.conf files its self.
+#       You could use this class to construct the directory structure but
+#       no actually use the Puppet generated app.conf.
+#
+#   - **basepath**
+#       We grab the Splunk user home directory from the Class['splunk::users'].
+#       If you do not control the Splunk user from within Puppet you will need
+#       override this value.
+#
+#   - **appname**
+#       Name of the Splunk application that Puppet creates.  This can be set
+#       because we plan to evolve this into a dynamic module that can deploy
+#       an arbitrary number of Splunk applications.  If you override this you
+#       need to remember to override if for every other piece of this module
+#       that can take a appname parameter.
 #
 # Requires:
 #
+#   A splunk user created in some fashion.  If you use Puppet then you can
+#   require Class['splunk::users'].
+#
 # Sample Usage:
+#
+#   class { 'splunk::app': }
 #
 class splunk::app(
   $enable    = true,
@@ -31,21 +61,22 @@ class splunk::app(
 
   $apppath = "${basepath}/etc/apps/${appname}"
 
-  file {
-    [
-      "${apppath}",
+  $paths = [
+      $apppath,
       "${apppath}/default",
-    ]:
+  ]
+
+  file { $paths:
       ensure => directory,
-      owner  => splunk,
-      group  => splunk,
+      owner  => 'splunk',
+      group  => 'splunk',
       mode   => '0755',
   }
 
   file { "${apppath}/default/app.conf":
     ensure => $ensure,
-    owner  => splunk,
-    group  => splunk,
+    owner  => 'splunk',
+    group  => 'splunk',
     mode   => '0644',
     content => template('splunk/appconf.erb'),
   }
