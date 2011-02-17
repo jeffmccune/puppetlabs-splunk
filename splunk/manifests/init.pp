a910e6a768f1618a7b055839eea66f7504d3afa7
Cody Herriges <c.a.herriges@gmail.com>
2011-02-16 16:57:42 -0800

    Documentation: init.pp
      Documented init.pp.  Moved to a spool dir that uses the PE default.

--- splunk/manifests/init.pp
+++ splunk/manifests/init.pp
@@ -1,8 +1,7 @@
 # Class: splunk
 #
-#   Puppet Labs Splunk module.
-#
-#   This module manages and configures Splunk
+#   This is the jumping off point for the Splunk module.  It provides some
+#   basic tooling that enables the proper generation of configs files.
 #
 #   Jeff McCune <jeff@puppetlabs.com>
 #   Cody Herriges <cody@puppetlabs.com>
@@ -10,20 +9,32 @@
 #
 # Parameters:
 #
-# Actions:
+#   - **fragbase**
+#       The base directory where file fragments are staged for config file
+#       generation.  We use a default that is current agreed upon by Puppet
+#       Labs Professional Services team.
 #
 # Requires:
 #
+#       User['puppet']
+#       Group['puppet']
+#       File['/var/opt/lib/pe-puppet']
+#         This is a loose requirement as it is configurable at class declaration
+#         time.
+#
 # Sample Usage:
 #
+#       class { 'splunk':
+#         fragbase => '/var/lib/puppet/spool',
+#       }
+#
 class splunk(
-  $fragbase = '/var/lib/puppet/spool'
+  $fragbase = '/var/opt/lib/pe-puppet/spool'
 ) {
 
   $fragpath = "${fragbase}/splunk.d"
 
-  # We will create a module for fragment
-  # directories.
+  # We will create a module for fragment directories.
 
   if ! defined(File[$fragbase]) {
     file { $fragbase:
