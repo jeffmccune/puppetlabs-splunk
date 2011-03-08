c63af9d252e783abc5d5d55db92b7f19357e91c1
Cody Herriges <c.a.herriges@gmail.com>
2011-03-07 17:36:32 -0800

    Fixed params regex.

--- splunk/manifests/params.pp
+++ splunk/manifests/params.pp
@@ -6,21 +6,21 @@
 #
 class splunk::params {
   $no_repo_provider = $operatingsystem ? {
-    /ubuntu|debian/        => 'dpkg',
-    /centos|fedora|redhat/ => 'rpm',
+    /(?i-mx:ubuntu|debian)/        => 'dpkg',
+    /(?i-mx:centos|fedora|redhat)/ => 'rpm',
   }
   $repo_provider = $operatingsystem ? {
-    /ubuntu|debian/        => 'apt',
-    /centos|fedora|redhat/ => 'yum',
+    /(?i-mx:ubuntu|debian)/        => 'apt',
+    /(?i-mx:centos|fedora|redhat)/ => 'yum',
   }
   $pkg_file = $operatingsystem ? {
-    /ubuntu|debian/        => $hardwaremodel ? {
+    /(?i-mx:ubuntu|debian)/ => $hardwaremodel ? {
       'x86_64' => 'splunk-4.1.7-95063-linux-2.6-amd64.deb',
       default  => 'splunk-4.1.7-95063-linux-2.6-intel.deb',
-    }
-    /centos|fedora|redhat/ => $hardwaremodel ? {
+    },
+    /(?i-mx:centos|fedora|redhat)/ => $hardwaremodel ? {
       'x86_64' => 'splunk-4.1.7-95063-linux-2.6-x86_64.rpm',
       default  => 'splunk-4.1.7-95063.i386.rpm',
-    }
+    },
   }
 }
